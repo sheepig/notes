a6bb27b7dac41865bcbc45c2df0737a6c7520ba6
qianyang.xie <qianyang.xie@vipshop.com>
2018-05-23 20:59:10 +0800

    console.time

--- demos/ch1/part3/index.js
+++ demos/ch1/part3/index.js
@@ -1,5 +1,11 @@
 var path = require('path');
-console.log('__dirname:', __dirname);
-console.log('__filename:', __filename);
-console.log('path.join():', path.join(__dirname, 'views', 'html'));
-module.exports = {};
+// console.log('__dirname:', __dirname);
+// console.log('__filename:', __filename);
+// console.log('path.join():', path.join(__dirname, 'views', 'html'));
+// module.exports = {};
+
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
+// process.stdin.on('data', function(text) {
+//     process.stdout.write(text.toUpperCase());
+// });
