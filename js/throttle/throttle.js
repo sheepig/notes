13730d64e1cb8d38252dc01ce19787b942e9bbb6
扬扬羊 <xqy@yangyanangdeAir.lan>
2018-06-17 00:17:38 +0800

    :heavy_check_mark:throttle

--- js/throttle/throttle.js
+++ js/throttle/throttle.js
@@ -29,9 +29,33 @@ function throttle(method, delay) {
         var args = arguments;
         if (!timer) {
             timer = setTimeout(function() {
-                method.apply(this, args);
+                method.apply(context, args);
                 timer = null;
             }, delay);
+        }
+    }
+}
+/**
+ * 定时器+时间戳
+ * @param   {function} method [节流的方法]
+ * @param   {number}   delay  [间隔执行时间]
+ * @returns {function}        []
+ */
+function throttle(method, delay) {
+    var prev = Date.now();
+    var timer = null;
+    return function() {
+        var now = Date.now();
+        var remain = delay - (now - prev);
+        var context = this;
+        var args = arguments;
+        if (remain <= 0) {
+            method.apply(context, args);
+            prev = Date.now();
+        } else {
+            timer = setTimeout(function(){
+                method.apply(context, args);
+            }, delay)
         }
     }
 }
